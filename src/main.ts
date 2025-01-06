5526fa17e1972b2014adf206fbe6585ce08908df
Dilani <karthigesudilani21@gmail.com>
2025-01-06 12:34:44 +0530

    Added swager api doc

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 dotenv.config();
 
@@ -17,6 +18,25 @@ async function bootstrap() {
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: false,
   });
+
+  // Swagger setup with bearer token security
+  const config = new DocumentBuilder()
+    .setTitle('API Documentation')
+    .setDescription('The API description')
+    .setVersion('1.0')
+    .addBearerAuth(
+      { type: 'http', scheme: 'bearer', bearerFormat: 'JWT' },
+      'JWT',
+    ) // Adding bearer token auth
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document, {
+    swaggerOptions: {
+      persistAuthorization: true, // Persist authorization across requests
+    },
+  });
+
   await app.listen(configService.get<number>('PORT') ?? 3000);
 }
 
